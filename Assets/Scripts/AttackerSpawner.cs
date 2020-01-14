bcb94f0f2abbc8a22da3e28bb7cf8b01e152b2a9
Sean Seefried <sean.seefried@gmail.com>
2020-01-15 08:47:12 +1100

    Add enemy spawners

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    bool spawn = true;
+
+    [SerializeField] GameObject lizardPrefab;
+    [SerializeField] float minDelay = 1f;
+    [SerializeField] float maxDelay = 5f;
+
+    IEnumerator Start()
+    {
+        while (spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minDelay, maxDelay));
+            Spawn();
+                            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Spawn()
+    {
+        Instantiate(lizardPrefab, transform.position, Quaternion.identity);
+    }
+}
