9b6a29a0edf6bbeb937ea5e30fcdf3eff69126a7
Sean Seefried <sean.seefried@gmail.com>
2020-01-19 12:44:32 +1000

    Add Cactus and make it shoot projectiles

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [Range(0f, 5f)] [SerializeField] float projectileSpeed = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * projectileSpeed * Time.deltaTime);
+    }
+}
