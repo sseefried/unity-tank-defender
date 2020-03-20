b94b60067ce3140d43739d7f84252b2f7b3b0834
Sean Seefried <sean.seefried@gmail.com>
2020-03-20 13:35:57 +1000

    Add tank and firing animation

---
+++ Assets/Scripts/Firing.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firing : MonoBehaviour
+{
+    [SerializeField] AudioClip firingSound;
+
+    public void PlayFireSound()
+    {
+        AudioSource.PlayClipAtPoint(firingSound, Camera.main.transform.position);
+    }
+    
+}
